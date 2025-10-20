503e5e2f03416290525518cb55b9fde10bdefd97
samboadas221 <samboadas2221@gmail.com>
2025-10-20 06:10:50 -0400

    Un regalo para una princesa... Espero que le guste

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,32 @@ class MyGame extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SoundManager.instance.playBackground('audio/music.opus');
-
-    Button buttonPlay = Button();
-    buttonPlay.setText("Jugar");
-    buttonPlay.setAudioOnClick("audio/click.mp3");
-
-    Button buttonSettings = Button();
-    buttonSettings.setText("Opciones");
-    buttonSettings.setAudioOnClick("audio/click.mp3");
-
-    Button buttonExit = Button();
-    buttonExit.setText("Salir");
-    buttonExit.setAudioOnClick("audio/click.mp3");
-    
-    Menu menuPrincipal = Menu(name: "Principal");
-    menuPrincipal.add(buttonPlay);
-    menuPrincipal.add(buttonSettings);
-    menuPrincipal.add(buttonExit);
-
-    menuPrincipal.matchButtonWidths();
-
-    menuPrincipal.setAsDefaultMenu();
+      
+      Button song1 = Button();
+      song1.setText("My Type");
+      song1.setAudioOnClick("audio/song1.m4a");
+      
+      Button song2 = Button();
+      song2.setText("Somewhere");
+      song2.setAudioOnClick("audio/song2.m4a");
+      
+      Button song3 = Button();
+      song3.setText("Amarte");
+      song3.setAudioOnClick("audio/song3.m4a");
+      
+      Button song4 = Button();
+      song4.setText("Matters");
+      song4.setAudioOnClick("audio/song4.m4a");
+      
+      Menu menu = Menu(name: "Principal");
+      menu.add(song1);
+      menu.add(song2);
+      menu.add(song3);
+      menu.add(song4);
+      
+      menu.matchButtonWidths();
+      menu.setAsDefaultMenu();
+      
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
