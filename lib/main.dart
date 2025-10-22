5735e9bb333b7175fbb140dbe5f87681b35cb209
samboadas221 <samboadas2221@gmail.com>
2025-10-22 00:29:43 -0400

    SoundManager rewrite. Retry #3

--- lib/main.dart
+++ lib/main.dart
@@ -33,19 +33,19 @@ class _MyGameState extends State<MyGame> {
     // ======= Crear botones =======
     Button song1 = Button();
     song1.setText("JUGAR");
-    song1.setAudioOnClick("audio/click.mp3", audioPlayer);
+    song1.setAudioOnClick("audio/click.mp3");
 
     Button song2 = Button();
     song2.setText("AjusTEs");
-    song2.setAudioOnClick("audio/click.mp3", audioPlayer);
+    song2.setAudioOnClick("audio/click.mp3");
 
     Button song3 = Button();
     song3.setText("opciones");
-    song3.setAudioOnClick("audio/click.mp3", audioPlayer);
+    song3.setAudioOnClick("audio/click.mp3");
 
     Button song4 = Button();
     song4.setText("sAliR");
-    song4.setAudioOnClick("audio/click.mp3", audioPlayer);
+    song4.setAudioOnClick("audio/click.mp3");
 
     Menu menu = Menu(name: "Principal");
     menu.add(song1);
