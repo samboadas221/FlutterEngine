03cd88f2c4726267fa0fea29b374a5a0173b95e3
samboadas221 <samboadas2221@gmail.com>
2025-10-27 13:00:18 -0400

    Adding Flame Camera

--- lib/GameDemo.dart
+++ lib/GameDemo.dart
@@ -1,15 +1,18 @@
 
 import 'dart:async';
-
 import 'package:flame/game.dart';
-
 import 'levels/level.dart';
 
 class GameDemo extends FlameGame{
   
+  late final CameraComponent camera;
+  final world = Level();
+  
   @override 
   FutureOr<void> onLoad() async{
-    add(Level());
+    camera = CameraComponent.withFixedResolution(world: world, width: 360, height: 600);
+    camera.viewFinder.anchor = Anchor.topLeft;
+    addAll([ camera, world ]);
     return super.onLoad();
   }
 }
